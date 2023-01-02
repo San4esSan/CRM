f1d5c7d4a26fe495610fb3710f0823fc09c08ece
mister.San4es <mister.San4es@list.ru>
2023-01-02 15:18:11 +0400

    fix error to cart.js

--- lesson-4.8/cart.js
+++ lesson-4.8/cart.js
@@ -1,61 +1,58 @@
-"use strict";
+'use strict';
 
 const cart = {
-    items: [],
-    count: 0,
+  items: [],
+  count: 0,
 
-    add(name, price, count = 1) {
-
-        this.items = [
-            {
-                name: 'телевизор',
-                price: 15000,
-                count: 2,
-            },
-            {
-                name: 'iphone',
-                price: 50000,
-                count: 1,
-            },
-            {
-                name: 'ноутбук',
-                price: 30000,
-                count: 1,
-            },
-        ]
-
-    },
+  add(name, price, count = 1) {
+    this.items = [
+      {
+        name: 'телевизор',
+        price: 15000,
+        count: 2,
+      },
+      {
+        name: 'iphone',
+        price: 50000,
+        count: 1,
+      },
+      {
+        name: 'ноутбук',
+        price: 30000,
+        count: 1,
+      },
+    ];
+  },
 
 
-    get totalPrice() {
-        return this.calculateItemPrice;
-    },
+  get totalPrice() {
+    return this.calculateItemPrice;
+  },
 
-    increaseCount(number) {
-        return this.count + number;
-    },
+  increaseCount(number) {
+    return this.count + number;
+  },
 
-    calculateItemPrice() {
-        this.summ = 0;
-        this.items.forEach(item => {
-            this.summ = this.summ + (item.price * item.count);
-            this.count += item.count;
-        })
+  calculateItemPrice() {
+    this.summ = 0;
+    this.items.forEach(item => {
+      this.summ += (item.price * item.count);
+      this.count += item.count;
+    });
 
-        return this.summ
-    },
+    return this.summ;
+  },
 
-    clear() {
-        this.items.length = 0;
-        // this.totalPrice = 0;
-        this.count = 0;
-    },
+  clear() {
+    this.items.length = 0;
+    this.count = 0;
+  },
 
-    print() {
-        cart.add();
-        console.log(JSON.stringify(cart.items));
-        console.log(cart.totalPrice(0));
-    },
-}
+  print() {
+    cart.add();
+    console.log(JSON.stringify(cart.items));
+    console.log(cart.totalPrice(0));
+  },
+};
 
 cart.print();
