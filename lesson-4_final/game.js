6a8cf09222422057106ba4caaa051880d47905fd
mister.San4es <mister.San4es@list.ru>
2023-01-20 23:39:11 +0400

    fix lesson-4 final

--- lesson-4_final/game.js
+++ lesson-4_final/game.js
@@ -13,46 +13,52 @@
       bot: 5,
     };
 
+    let playerChoise = 0;
+    let playerNumber = 0;
+
     return function start() {
-      const playerChoise = +(prompt(`Загадай число от 1 до ${result.user}`));
 
-      let playerNumber = 0;
+      while (result.bot >= 1 && result.user >= 1) {
+        const checkNumber = () => {
+          playerChoise = +(prompt(`Загадай число от 1 до ${result.user}`));
 
-      if (playerChoise % 2 === 0) {
-        playerNumber = 2;
-      } else {
-        playerNumber = 1;
-      }
+          while (Number.isNaN(playerChoise) && playerChoise <= result.user) {
+            playerChoise = +(prompt(`Загадай число от 1 до ${result.user}`));
+          }
 
-      const computerChoise = getRandomIntInclusive(1, 2);
-
-      if (playerChoise >= 1 && playerChoise <= result.user) {
-        if (computerChoise === playerNumber) {
-          result.bot += playerChoise;
-          result.user -= playerChoise;
-          console.log(result.user, result.bot);
-          alert(`Bot-угадал\nСЧЕТ:\nBot: ${result.bot}\nИгрок: ${result.user}`);
-        } else {
-          if (playerChoise > result.bot) {
-            result.user += result.bot;
-            result.bot -= result.bot;
-            console.log(result.user, result.bot);
-            alert(`Bot-не угадал\nСЧЕТ:\nBot: ${result.bot}\nИгрок: ${result.user}`);
+          if (playerChoise % 2 === 0) {
+            playerNumber = 2;
           } else {
-            result.user += playerChoise;
-            result.bot -= playerChoise;
-            console.log(result.user, result.bot);
-            alert(`Bot-не угадал\nСЧЕТ:\nBot: ${result.bot}\nИгрок: ${result.user}`);
+            playerNumber = 1;
           }
         }
-      } else {
-        prompt(`Загадай число от 1 до ${result.user}`);
-        start();
+        checkNumber();
+
+        const computerChoise = getRandomIntInclusive(1, 2);
+
+        if (playerChoise >= 1 && playerChoise <= result.user) {
+          if (computerChoise === playerNumber) {
+            result.bot += playerChoise;
+            result.user -= playerChoise;
+            console.log(result.user, result.bot);
+            alert(`Bot-угадал\nСЧЕТ:\nBot: ${result.bot}\nИгрок: ${result.user}`);
+          } else {
+            if (playerChoise > result.bot) {
+              result.user += result.bot;
+              result.bot -= result.bot;
+              console.log(result.user, result.bot);
+              alert(`Bot-не угадал\nСЧЕТ:\nBot: ${result.bot}\nИгрок: ${result.user}`);
+            } else {
+              result.user += playerChoise;
+              result.bot -= playerChoise;
+              console.log(result.user, result.bot);
+              alert(`Bot-не угадал\nСЧЕТ:\nBot: ${result.bot}\nИгрок: ${result.user}`);
+            }
+          }
+        }
       }
 
-      if (result.bot >= 1 && result.user >= 1) {
-        start();
-      } else {
+      if (result.bot === 0 || result.user === 0) {
         alert(`Game over!\nСЧЕТ:\nBot: ${result.bot}\nИгрок: ${result.user}`);
       }
     };
