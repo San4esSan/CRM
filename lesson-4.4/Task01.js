34affe063716f8e988fd86f52f6815a4aa592082
mister.San4es <mister.San4es@list.ru>
2023-01-25 22:55:28 +0400

    added lesson-5.2

--- lesson-4.4/Task01.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-const getConvert = (many) => {
-    const eu = 1.2
-    const usd = 73
-    return many * eu * usd
-};
-
-console.log(getConvert(10));
--- lesson-4.5/Task01.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-for (let i = 1; i <= 10; i++) {
-    console.log('--------------------------')
-    for (let k = 1; k <= 10; k++) {
-        console.log(`${i} ** ${k} = ${i ** k}`)
-    }
-}
