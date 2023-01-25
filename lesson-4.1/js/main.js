d36c9896676c6234c26e4803365ee3981cbe4c7f
mister.San4es <mister.San4es@list.ru>
2023-01-25 22:52:00 +0400

    added lesson-5.2

---
+++ lesson-4.1/js/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const modalTitle = document.querySelector('.modal__title');
+const titleId = document.querySelector('.title-id');
+const modalTitleSvg = document.querySelector('.modal__title-svg');
+const form = document.querySelector('.form');
+const formUnitsCheck = document.querySelector('.form__units-check');
+const formDiscount = document.querySelector('.form__discount');
+const formPrice = document.querySelector('.form__price-text');
+
+console.log(form)
